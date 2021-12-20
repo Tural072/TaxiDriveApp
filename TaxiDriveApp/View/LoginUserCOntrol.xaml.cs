72c6f915d8a7faecb8297020b11fd5915c03038d
tahi_ri96 <tahi_ri96@STHQ0127-05>
2021-12-20 14:08:14 +0400

    Add project files.

---
+++ TaxiDriveApp/View/LoginUserCOntrol.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaxiDriveApp.Models;
+using TaxiDriveApp.ViewModel;
+
+namespace TaxiDriveApp.View
+{
+    /// <summary>
+    /// Interaction logic for LoginUserCOntrol.xaml
+    /// </summary>
+    public partial class LoginUserCOntrol : UserControl
+    {
+        public LoginUserCOntrol()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginUserControlViewModel(this);
+        }
+    }
+}
---
+++ TaxiDriveApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using TaxiDriveApp.Commands;
+using TaxiDriveApp.View;
+
+namespace TaxiDriveApp.ViewModel
+{
+   public class MainWindowViewModel:BaseViewModel
+    {
+        public RelayCommand MouseDown { get; set; }
+        public RelayCommand MouseDownForReset { get; set; }
+        public RelayCommand MouseDownForSend { get; set; }
+        public RelayCommand MouseDownForRegisterNextButton { get; set; }
+
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+            MouseDown = new RelayCommand((sender) =>
+            {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = mainWindow.FindResource("Storyboard1") as Storyboard;
+                beginStoryboard.Begin();
+
+            });
+            MouseDownForReset = new RelayCommand((sender) => {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = mainWindow.FindResource("ResetStoryBoard") as Storyboard;
+                beginStoryboard.Begin();
+
+
+            });
+            MouseDownForSend = new RelayCommand((sender) =>
+            {
+                //mainWindow.Login.Visibility = Visibility.Visible;
+
+                for (int i = 0; i < mainWindow.ResetPassword.Children.Count; i++)
+                {
+                    mainWindow.ResetPassword.Children.RemoveAt(i);
+                }
+
+                for (int i = 0; i < mainWindow.Login.Children.Count; i++)
+                {
+                    mainWindow.Login.Children.RemoveAt(i);
+                }
+                for (int i = 0; i < mainWindow.Register.Children.Count; i++)
+                {
+                    mainWindow.Register.Children.RemoveAt(i);
+                }
+                LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
+                mainWindow.MainGrid.Children.Add(loginUserCOntrol);
+            });
+            MouseDownForRegisterNextButton = new RelayCommand((sender) => {
+
+
+                for (int i = 0; i < mainWindow.ResetPassword.Children.Count; i++)
+                {
+                    mainWindow.ResetPassword.Children.RemoveAt(i);
+                }
+
+                for (int i = 0; i < mainWindow.Login.Children.Count; i++)
+                {
+                    mainWindow.Login.Children.RemoveAt(i);
+                }
+                for (int i = 0; i < mainWindow.Register.Children.Count; i++)
+                {
+                    mainWindow.Register.Children.RemoveAt(i);
+                }
+                LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
+                mainWindow.MainGrid.Children.Add(loginUserCOntrol);
+
+
+            });
+        }
+    }
+}
